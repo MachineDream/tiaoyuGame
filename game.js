71be56d4af8c80f8b848b311a6a1068e967af794
tiaoyu <txydsx@live.com>
2023-01-08 22:40:48 +0800

    适配最新稳定版本微信小程序开发工具

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
-// import './js/libs/weapp-adapter'
-// import './js/libs/symbol'
-import './js/libs/adapter/index';
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+// import './js/libs/adapter/index';
 
 import Main from './js/main'
 
